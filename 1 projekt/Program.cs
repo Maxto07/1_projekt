460979d1aa0b705795895b506210d17671fb8a7b
1Tp <1Tp@DESKTOP-ELSJS1V>
2022-11-30 12:19:18 +0100

    Dodaj pliki projektów.

---
+++ 1 projekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connect to Github!");
+        }
+    }
+}
